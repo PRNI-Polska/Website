58a09a119794b17f72193ad03e7792179d53208b
Anonymous <anonymous@users.noreply.github.com>
2020-01-01 00:00:00 +0000

    International Wing: Add email notifications for registrations
    - Uses same Resend email service as contact form
    - Sends formatted HTML email to admin (CONTACT_EMAIL)
    - Includes all registration details in table format
    - Falls back to console.log if no RESEND_API_KEY
    - Added rate limiting (3 requests per 30 min per IP)
    - Reply-to set to registrant's email for easy follow-up

--- app/api/international-join/route.ts
+++ app/api/international-join/route.ts
@@ -1,5 +1,9 @@
 // file: app/api/international-join/route.ts
 import { NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
+import { checkRateLimit } from "@/lib/utils";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 interface InternationalJoinData {
   name: string;
@@ -11,14 +15,132 @@ interface InternationalJoinData {
   consent: boolean;
 }
 
+const INTEREST_LABELS: Record<string, string> = {
+  translation: "Translation & Localization",
+  outreach: "Outreach & Social Media",
+  events: "Events & Coordination",
+  research: "Research & Analysis",
+  other: "Other",
+};
+
+async function sendNotificationEmail(data: InternationalJoinData) {
+  const interestLabel = INTEREST_LABELS[data.interest] || data.interest;
+  
+  // If no API key configured, just log
+  if (!process.env.RESEND_API_KEY) {
+    console.log("=== International Wing Registration ===");
+    console.log("Name:", data.name);
+    console.log("Email:", data.email);
+    console.log("Country:", data.country);
+    console.log("Languages:", data.languages || "Not specified");
+    console.log("Interest:", interestLabel);
+    console.log("Message:", data.message || "No message");
+    console.log("Timestamp:", new Date().toISOString());
+    console.log("========================================");
+    return { success: true };
+  }
+
+  // Send email to admin via Resend
+  const { error } = await resend.emails.send({
+    from: "PRNI Website <onboarding@resend.dev>",
+    to: process.env.CONTACT_EMAIL || "prni.official@gmail.com",
+    replyTo: data.email,
+    subject: `[PRNI International Wing] New Affiliate Registration: ${data.name}`,
+    text: `New International Wing Registration
+
+Name: ${data.name}
+Email: ${data.email}
+Country: ${data.country}
+Languages: ${data.languages || "Not specified"}
+Area of Interest: ${interestLabel}
+
+Message:
+${data.message || "No message provided"}
+
+---
+Submitted: ${new Date().toISOString()}
+Note: This person has acknowledged that participation does not constitute party membership.`,
+    html: `
+      <h2>New International Wing Registration</h2>
+      <table style="border-collapse: collapse; width: 100%; max-width: 600px;">
+        <tr>
+          <td style="padding: 8px; border-bottom: 1px solid #eee; font-weight: bold; width: 140px;">Name:</td>
+          <td style="padding: 8px; border-bottom: 1px solid #eee;">${data.name}</td>
+        </tr>
+        <tr>
+          <td style="padding: 8px; border-bottom: 1px solid #eee; font-weight: bold;">Email:</td>
+          <td style="padding: 8px; border-bottom: 1px solid #eee;"><a href="mailto:${data.email}">${data.email}</a></td>
+        </tr>
+        <tr>
+          <td style="padding: 8px; border-bottom: 1px solid #eee; font-weight: bold;">Country:</td>
+          <td style="padding: 8px; border-bottom: 1px solid #eee;">${data.country}</td>
+        </tr>
+        <tr>
+          <td style="padding: 8px; border-bottom: 1px solid #eee; font-weight: bold;">Languages:</td>
+          <td style="padding: 8px; border-bottom: 1px solid #eee;">${data.languages || "<em>Not specified</em>"}</td>
+        </tr>
+        <tr>
+          <td style="padding: 8px; border-bottom: 1px solid #eee; font-weight: bold;">Interest Area:</td>
+          <td style="padding: 8px; border-bottom: 1px solid #eee;">${interestLabel}</td>
+        </tr>
+      </table>
+      
+      ${data.message ? `
+        <h3 style="margin-top: 20px;">Message:</h3>
+        <p style="background: #f5f5f5; padding: 15px; border-radius: 5px;">${data.message.replace(/\n/g, "<br>")}</p>
+      ` : ""}
+      
+      <hr style="margin: 20px 0; border: none; border-top: 1px solid #ddd;">
+      <p style="color: #666; font-size: 12px;">
+        Submitted: ${new Date().toISOString()}<br>
+        <em>This person has acknowledged that participation does not constitute party membership.</em>
+      </p>
+    `,
+  });
+
+  if (error) {
+    console.error("Failed to send notification email:", error);
+    throw new Error("Failed to send notification email");
+  }
+
+  return { success: true };
+}
+
 export async function POST(request: NextRequest) {
   try {
+    // Get client IP for rate limiting
+    const ip = request.headers.get("x-forwarded-for") || 
+               request.headers.get("x-real-ip") || 
+               "unknown";
+
+    // Rate limiting: 3 requests per 30 minutes per IP
+    const rateLimit = checkRateLimit(`intl-join:${ip}`, 3, 30 * 60 * 1000);
+    
+    if (!rateLimit.allowed) {
+      return NextResponse.json(
+        { error: "Too many requests. Please try again later." },
+        { 
+          status: 429,
+          headers: {
+            "Retry-After": Math.ceil(rateLimit.resetIn / 1000).toString(),
+          },
+        }
+      );
+    }
+
     const data: InternationalJoinData = await request.json();
 
     // Validate required fields
-    if (!data.name || !data.email || !data.country || !data.interest) {
+    if (!data.name?.trim()) {
       return NextResponse.json(
-        { error: "Missing required fields" },
+        { error: "Name is required" },
+        { status: 400 }
+      );
+    }
+
+    if (!data.email?.trim()) {
+      return NextResponse.json(
+        { error: "Email is required" },
         { status: 400 }
       );
     }
@@ -32,6 +154,20 @@ export async function POST(request: NextRequest) {
       );
     }
 
+    if (!data.country?.trim()) {
+      return NextResponse.json(
+        { error: "Country is required" },
+        { status: 400 }
+      );
+    }
+
+    if (!data.interest?.trim()) {
+      return NextResponse.json(
+        { error: "Area of interest is required" },
+        { status: 400 }
+      );
+    }
+
     // Validate consent
     if (!data.consent) {
       return NextResponse.json(
@@ -40,24 +176,9 @@ export async function POST(request: NextRequest) {
       );
     }
 
-    // Log the submission (for development/debugging)
-    console.log("International Wing Registration:", {
-      name: data.name,
-      email: data.email,
-      country: data.country,
-      languages: data.languages || "Not specified",
-      interest: data.interest,
-      message: data.message || "No message",
-      timestamp: new Date().toISOString(),
-    });
+    // Send notification email
+    await sendNotificationEmail(data);
 
-    // In the future, this could:
-    // 1. Save to database
-    // 2. Send email notification to admin
-    // 3. Send confirmation email to user
-    // 4. Add to newsletter/CRM system
-
-    // For now, just return success
     return NextResponse.json({
       success: true,
       message: "Registration received successfully",
@@ -65,7 +186,7 @@ export async function POST(request: NextRequest) {
   } catch (error) {
     console.error("International join error:", error);
     return NextResponse.json(
-      { error: "Internal server error" },
+      { error: "Failed to process registration. Please try again." },
       { status: 500 }
     );
   }

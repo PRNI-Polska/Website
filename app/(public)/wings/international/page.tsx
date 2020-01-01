bcafc443e21e94ac18f160c71030e77bda974fd5
Anonymous <anonymous@users.noreply.github.com>
2020-01-01 00:00:00 +0000

    Refactor Wings Gateway: premium motion system + disciplined design
    Motion System (standardized):
    - --dur-1: 220ms (micro hover)
    - --dur-2: 320ms (hover/press)
    - --dur-3: 700ms (page/panel transitions)
    - --dur-4: 1000ms (hero entrance)
    - --ease-out: cubic-bezier(0.22, 1, 0.36, 1)
    - --ease-enter: cubic-bezier(0.16, 1, 0.3, 1)
    
    Visual Design:
    - Remove heavy gradients, use off-white bg with subtle vignette + noise
    - Hero: Eyebrow â†’ H1 'Wings' â†’ Subtitle 'Three branches. One movement.'
    - Panels: minimal border, subtle shadow, strong typography
    - Custom monochrome SVG icons with consistent stroke weight
    - Remove 'Read more' element, minimal scroll indicator
    
    Interactions:
    - Panel hover: translateY(-6px) scale(1.01), smooth shadow
    - Panel takeover: selected scales, others fade/slide away
    - Female Wing: visible but disabled with tooltip on click
    - Focus states match hover intent
    
    Accessibility:
    - prefers-reduced-motion: disable transforms/blur, keep fades
    - Keyboard navigable panels with visible focus states
    - Large click targets

--- app/(public)/wings/international/page.tsx
+++ app/(public)/wings/international/page.tsx
@@ -2,113 +2,132 @@
 "use client";
 
 import Link from "next/link";
-import { ArrowLeft, Globe, Languages, Handshake, Newspaper } from "lucide-react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useI18n } from "@/lib/i18n";
 
+// Minimal icons
+const TranslateIcon = ({ className }: { className?: string }) => (
+  <svg className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M5 8l6 6" />
+    <path d="M4 14l6-6 2-3" />
+    <path d="M2 5h12" />
+    <path d="M7 2v3" />
+    <path d="M22 22l-5-10-5 10" />
+    <path d="M14 18h6" />
+  </svg>
+);
+
+const HandshakeIcon = ({ className }: { className?: string }) => (
+  <svg className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M20.42 4.58a5.4 5.4 0 0 0-7.65 0l-.77.78-.77-.78a5.4 5.4 0 0 0-7.65 0C1.46 6.7 1.33 10.28 4 13l8 8 8-8c2.67-2.72 2.54-6.3.42-8.42z" />
+  </svg>
+);
+
+const NewspaperIcon = ({ className }: { className?: string }) => (
+  <svg className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-2 2Zm0 0a2 2 0 0 1-2-2v-9c0-1.1.9-2 2-2h2" />
+    <path d="M18 14h-8" />
+    <path d="M15 18h-5" />
+    <path d="M10 6h8v4h-8V6Z" />
+  </svg>
+);
+
+const GlobeIcon = ({ className }: { className?: string }) => (
+  <svg className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+    <circle cx="12" cy="12" r="10" />
+    <line x1="2" y1="12" x2="22" y2="12" />
+    <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" />
+  </svg>
+);
+
 export default function InternationalWingPage() {
-  const { t } = useI18n();
+  const { t, locale } = useI18n();
 
   const responsibilities = [
-    { key: "wings.international.responsibilities.1", icon: Languages },
-    { key: "wings.international.responsibilities.2", icon: Handshake },
-    { key: "wings.international.responsibilities.3", icon: Newspaper },
-    { key: "wings.international.responsibilities.4", icon: Globe },
+    { key: "wings.international.responsibilities.1", Icon: TranslateIcon },
+    { key: "wings.international.responsibilities.2", Icon: HandshakeIcon },
+    { key: "wings.international.responsibilities.3", Icon: NewspaperIcon },
+    { key: "wings.international.responsibilities.4", Icon: GlobeIcon },
   ];
+
+  const backText = locale === "pl" ? "Powrót" : locale === "de" ? "Zurück" : "Back";
 
   return (
     <div className="min-h-screen">
       {/* Hero */}
-      <section className="relative py-32 md:py-40 overflow-hidden">
-        <div className="absolute inset-0 bg-gradient-to-b from-primary/5 via-transparent to-transparent" />
-        
-        <div className="container-custom relative">
-          {/* Back link */}
+      <section className="py-24 md:py-32">
+        <div className="container-custom max-w-3xl">
+          {/* Back */}
           <Link 
             href="/"
-            className="inline-flex items-center gap-2 text-sm text-muted-foreground hover:text-primary transition-colors mb-12 group"
+            className="reveal-section reveal-delay-1 inline-flex items-center gap-2 text-sm text-muted-foreground hover:text-foreground transition-colors mb-12 group"
           >
-            <ArrowLeft className="w-4 h-4 transition-transform group-hover:-translate-x-1" />
-            {t("wings.back")}
+            <ArrowLeft className="w-4 h-4 transition-transform group-hover:-translate-x-1" style={{ transitionDuration: 'var(--dur-2)', transitionTimingFunction: 'var(--ease-out)' }} />
+            {backText}
           </Link>
 
-          <div className="max-w-3xl">
-            <h1 className="text-4xl md:text-5xl lg:text-6xl font-heading font-bold tracking-tight mb-6 panel-reveal panel-reveal-1">
-              {t("wings.international.title")}
-            </h1>
-            <p className="text-xl md:text-2xl text-muted-foreground panel-reveal panel-reveal-2">
-              {t("wings.international.tagline")}
-            </p>
-            <div className="w-24 h-1 bg-primary mt-8 line-reveal" />
-          </div>
+          <h1 className="reveal-section reveal-delay-2 text-4xl md:text-5xl font-heading font-bold tracking-tight text-foreground mb-4">
+            {t("wings.international.title")}
+          </h1>
+          <p className="reveal-section reveal-delay-3 text-lg text-muted-foreground">
+            {t("wings.international.tagline")}
+          </p>
         </div>
       </section>
 
       {/* Purpose */}
-      <section className="py-20 bg-muted/30">
-        <div className="container-custom">
-          <div className="max-w-3xl mx-auto">
-            <h2 className="text-2xl md:text-3xl font-heading font-semibold mb-6">
-              {t("wings.international.purpose.title")}
-            </h2>
-            <p className="text-lg text-muted-foreground leading-relaxed">
-              {t("wings.international.purpose.text")}
-            </p>
-          </div>
+      <section className="py-16 border-t border-border">
+        <div className="container-custom max-w-3xl">
+          <h2 className="reveal-section reveal-delay-1 text-xs font-medium tracking-[0.2em] uppercase text-muted-foreground mb-4">
+            {t("wings.international.purpose.title")}
+          </h2>
+          <p className="reveal-section reveal-delay-2 text-lg text-foreground leading-relaxed">
+            {t("wings.international.purpose.text")}
+          </p>
         </div>
       </section>
 
       {/* Responsibilities */}
-      <section className="py-20">
-        <div className="container-custom">
-          <div className="max-w-3xl mx-auto">
-            <h2 className="text-2xl md:text-3xl font-heading font-semibold mb-10">
-              {t("wings.international.responsibilities.title")}
-            </h2>
-            <ul className="space-y-6">
-              {responsibilities.map((item, index) => {
-                const Icon = item.icon;
-                return (
-                  <li 
-                    key={item.key}
-                    className="flex items-start gap-4 panel-reveal"
-                    style={{ animationDelay: `${index * 100}ms` }}
-                  >
-                    <div className="w-10 h-10 rounded-lg bg-primary/10 flex items-center justify-center flex-shrink-0">
-                      <Icon className="w-5 h-5 text-primary" />
-                    </div>
-                    <span className="text-lg pt-2">{t(item.key)}</span>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+      <section className="py-16 border-t border-border">
+        <div className="container-custom max-w-3xl">
+          <h2 className="reveal-section reveal-delay-1 text-xs font-medium tracking-[0.2em] uppercase text-muted-foreground mb-8">
+            {t("wings.international.responsibilities.title")}
+          </h2>
+          <ul className="space-y-4">
+            {responsibilities.map((item, index) => (
+              <li 
+                key={item.key}
+                className="reveal-section flex items-start gap-4"
+                style={{ animationDelay: `${(index + 1) * 80}ms` }}
+              >
+                <item.Icon className="w-5 h-5 text-muted-foreground flex-shrink-0 mt-0.5" />
+                <span className="text-foreground">{t(item.key)}</span>
+              </li>
+            ))}
+          </ul>
         </div>
       </section>
 
-      {/* Engage CTA */}
-      <section className="py-20 bg-primary text-primary-foreground">
-        <div className="container-custom">
-          <div className="max-w-3xl mx-auto text-center">
-            <h2 className="text-2xl md:text-3xl font-heading font-semibold mb-4">
-              {t("wings.international.engage.title")}
-            </h2>
-            <p className="text-lg opacity-90 mb-8">
-              {t("wings.international.engage.text")}
-            </p>
-            <div className="flex flex-col sm:flex-row gap-4 justify-center">
-              <Button size="lg" variant="secondary" asChild>
-                <Link href="/contact">{t("wings.joinCta")}</Link>
-              </Button>
-              <Button 
-                size="lg" 
-                variant="outline"
-                className="border-2 border-white text-white bg-transparent hover:bg-white/20"
-                asChild
-              >
-                <Link href="/contact">{t("wings.contactCta")}</Link>
-              </Button>
-            </div>
+      {/* Engage */}
+      <section className="py-20 border-t border-border bg-muted/30">
+        <div className="container-custom max-w-3xl">
+          <h2 className="reveal-section reveal-delay-1 text-xs font-medium tracking-[0.2em] uppercase text-muted-foreground mb-4">
+            {t("wings.international.engage.title")}
+          </h2>
+          <p className="reveal-section reveal-delay-2 text-lg text-foreground mb-8">
+            {t("wings.international.engage.text")}
+          </p>
+          <div className="reveal-section reveal-delay-3 flex flex-wrap gap-3">
+            <Button asChild className="hover-lift">
+              <Link href="/contact">
+                {t("wings.joinCta")}
+                <ArrowRight className="ml-1.5 w-4 h-4" />
+              </Link>
+            </Button>
+            <Button variant="outline" asChild className="hover-lift">
+              <Link href="/contact">{t("wings.contactCta")}</Link>
+            </Button>
           </div>
         </div>
       </section>
--- app/(public)/wings/main/page.tsx
+++ app/(public)/wings/main/page.tsx
@@ -2,113 +2,143 @@
 "use client";
 
 import Link from "next/link";
-import { ArrowLeft, Users, Target, Building2, Megaphone } from "lucide-react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useI18n } from "@/lib/i18n";
 
+// Minimal icons
+const TargetIcon = ({ className }: { className?: string }) => (
+  <svg className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+    <circle cx="12" cy="12" r="10" />
+    <circle cx="12" cy="12" r="6" />
+    <circle cx="12" cy="12" r="2" />
+  </svg>
+);
+
+const StructureIcon = ({ className }: { className?: string }) => (
+  <svg className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+    <rect x="3" y="3" width="7" height="7" />
+    <rect x="14" y="3" width="7" height="7" />
+    <rect x="14" y="14" width="7" height="7" />
+    <rect x="3" y="14" width="7" height="7" />
+  </svg>
+);
+
+const UsersIcon = ({ className }: { className?: string }) => (
+  <svg className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />
+    <circle cx="9" cy="7" r="4" />
+    <path d="M23 21v-2a4 4 0 0 0-3-3.87" />
+    <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+  </svg>
+);
+
+const CalendarIcon = ({ className }: { className?: string }) => (
+  <svg className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+    <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
+    <line x1="16" y1="2" x2="16" y2="6" />
+    <line x1="8" y1="2" x2="8" y2="6" />
+    <line x1="3" y1="10" x2="21" y2="10" />
+  </svg>
+);
+
+const PlatformIcon = ({ className }: { className?: string }) => (
+  <svg className={className} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+    <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
+    <polyline points="14,2 14,8 20,8" />
+    <line x1="16" y1="13" x2="8" y2="13" />
+    <line x1="16" y1="17" x2="8" y2="17" />
+    <polyline points="10,9 9,9 8,9" />
+  </svg>
+);
+
 export default function MainWingPage() {
-  const { t } = useI18n();
+  const { t, locale } = useI18n();
 
   const responsibilities = [
-    { key: "wings.main.responsibilities.1", icon: Target },
-    { key: "wings.main.responsibilities.2", icon: Building2 },
-    { key: "wings.main.responsibilities.3", icon: Users },
-    { key: "wings.main.responsibilities.4", icon: Megaphone },
+    { key: "wings.main.responsibilities.1", Icon: PlatformIcon },
+    { key: "wings.main.responsibilities.2", Icon: StructureIcon },
+    { key: "wings.main.responsibilities.3", Icon: UsersIcon },
+    { key: "wings.main.responsibilities.4", Icon: CalendarIcon },
   ];
+
+  const backText = locale === "pl" ? "Powrót" : locale === "de" ? "Zurück" : "Back";
 
   return (
     <div className="min-h-screen">
       {/* Hero */}
-      <section className="relative py-32 md:py-40 overflow-hidden">
-        <div className="absolute inset-0 bg-gradient-to-b from-primary/5 via-transparent to-transparent" />
-        
-        <div className="container-custom relative">
-          {/* Back link */}
+      <section className="py-24 md:py-32">
+        <div className="container-custom max-w-3xl">
+          {/* Back */}
           <Link 
             href="/"
-            className="inline-flex items-center gap-2 text-sm text-muted-foreground hover:text-primary transition-colors mb-12 group"
+            className="reveal-section reveal-delay-1 inline-flex items-center gap-2 text-sm text-muted-foreground hover:text-foreground transition-colors mb-12 group"
           >
-            <ArrowLeft className="w-4 h-4 transition-transform group-hover:-translate-x-1" />
-            {t("wings.back")}
+            <ArrowLeft className="w-4 h-4 transition-transform group-hover:-translate-x-1" style={{ transitionDuration: 'var(--dur-2)', transitionTimingFunction: 'var(--ease-out)' }} />
+            {backText}
           </Link>
 
-          <div className="max-w-3xl">
-            <h1 className="text-4xl md:text-5xl lg:text-6xl font-heading font-bold tracking-tight mb-6 panel-reveal panel-reveal-1">
-              {t("wings.main.title")}
-            </h1>
-            <p className="text-xl md:text-2xl text-muted-foreground panel-reveal panel-reveal-2">
-              {t("wings.main.tagline")}
-            </p>
-            <div className="w-24 h-1 bg-primary mt-8 line-reveal" />
-          </div>
+          <h1 className="reveal-section reveal-delay-2 text-4xl md:text-5xl font-heading font-bold tracking-tight text-foreground mb-4">
+            {t("wings.main.title")}
+          </h1>
+          <p className="reveal-section reveal-delay-3 text-lg text-muted-foreground">
+            {t("wings.main.tagline")}
+          </p>
         </div>
       </section>
 
       {/* Purpose */}
-      <section className="py-20 bg-muted/30">
-        <div className="container-custom">
-          <div className="max-w-3xl mx-auto">
-            <h2 className="text-2xl md:text-3xl font-heading font-semibold mb-6">
-              {t("wings.main.purpose.title")}
-            </h2>
-            <p className="text-lg text-muted-foreground leading-relaxed">
-              {t("wings.main.purpose.text")}
-            </p>
-          </div>
+      <section className="py-16 border-t border-border">
+        <div className="container-custom max-w-3xl">
+          <h2 className="reveal-section reveal-delay-1 text-xs font-medium tracking-[0.2em] uppercase text-muted-foreground mb-4">
+            {t("wings.main.purpose.title")}
+          </h2>
+          <p className="reveal-section reveal-delay-2 text-lg text-foreground leading-relaxed">
+            {t("wings.main.purpose.text")}
+          </p>
         </div>
       </section>
 
       {/* Responsibilities */}
-      <section className="py-20">
-        <div className="container-custom">
-          <div className="max-w-3xl mx-auto">
-            <h2 className="text-2xl md:text-3xl font-heading font-semibold mb-10">
-              {t("wings.main.responsibilities.title")}
-            </h2>
-            <ul className="space-y-6">
-              {responsibilities.map((item, index) => {
-                const Icon = item.icon;
-                return (
-                  <li 
-                    key={item.key}
-                    className="flex items-start gap-4 panel-reveal"
-                    style={{ animationDelay: `${index * 100}ms` }}
-                  >
-                    <div className="w-10 h-10 rounded-lg bg-primary/10 flex items-center justify-center flex-shrink-0">
-                      <Icon className="w-5 h-5 text-primary" />
-                    </div>
-                    <span className="text-lg pt-2">{t(item.key)}</span>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+      <section className="py-16 border-t border-border">
+        <div className="container-custom max-w-3xl">
+          <h2 className="reveal-section reveal-delay-1 text-xs font-medium tracking-[0.2em] uppercase text-muted-foreground mb-8">
+            {t("wings.main.responsibilities.title")}
+          </h2>
+          <ul className="space-y-4">
+            {responsibilities.map((item, index) => (
+              <li 
+                key={item.key}
+                className="reveal-section flex items-start gap-4"
+                style={{ animationDelay: `${(index + 1) * 80}ms` }}
+              >
+                <item.Icon className="w-5 h-5 text-muted-foreground flex-shrink-0 mt-0.5" />
+                <span className="text-foreground">{t(item.key)}</span>
+              </li>
+            ))}
+          </ul>
         </div>
       </section>
 
-      {/* Engage CTA */}
-      <section className="py-20 bg-primary text-primary-foreground">
-        <div className="container-custom">
-          <div className="max-w-3xl mx-auto text-center">
-            <h2 className="text-2xl md:text-3xl font-heading font-semibold mb-4">
-              {t("wings.main.engage.title")}
-            </h2>
-            <p className="text-lg opacity-90 mb-8">
-              {t("wings.main.engage.text")}
-            </p>
-            <div className="flex flex-col sm:flex-row gap-4 justify-center">
-              <Button size="lg" variant="secondary" asChild>
-                <Link href="/contact">{t("wings.joinCta")}</Link>
-              </Button>
-              <Button 
-                size="lg" 
-                variant="outline"
-                className="border-2 border-white text-white bg-transparent hover:bg-white/20"
-                asChild
-              >
-                <Link href="/contact">{t("wings.contactCta")}</Link>
-              </Button>
-            </div>
+      {/* Engage */}
+      <section className="py-20 border-t border-border bg-muted/30">
+        <div className="container-custom max-w-3xl">
+          <h2 className="reveal-section reveal-delay-1 text-xs font-medium tracking-[0.2em] uppercase text-muted-foreground mb-4">
+            {t("wings.main.engage.title")}
+          </h2>
+          <p className="reveal-section reveal-delay-2 text-lg text-foreground mb-8">
+            {t("wings.main.engage.text")}
+          </p>
+          <div className="reveal-section reveal-delay-3 flex flex-wrap gap-3">
+            <Button asChild className="hover-lift">
+              <Link href="/contact">
+                {t("wings.joinCta")}
+                <ArrowRight className="ml-1.5 w-4 h-4" />
+              </Link>
+            </Button>
+            <Button variant="outline" asChild className="hover-lift">
+              <Link href="/contact">{t("wings.contactCta")}</Link>
+            </Button>
           </div>
         </div>
       </section>

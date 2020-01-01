a7a2da07a37f005c4cc7b0eaa1b6d71d01283cae
Anonymous <anonymous@users.noreply.github.com>
2020-01-01 00:00:00 +0000

    Add manifesto content (PL/EN/DE) and render manifesto page

--- app/(public)/manifesto/page.tsx
+++ app/(public)/manifesto/page.tsx
@@ -1,37 +1,34 @@
 // file: app/(public)/manifesto/page.tsx
 "use client";
 
-import { Construction } from "lucide-react";
 import { Card, CardContent } from "@/components/ui/card";
+import { manifestoContent } from "@/content/manifesto";
 import { useI18n } from "@/lib/i18n";
 
 export default function ManifestoPage() {
   const { locale } = useI18n();
+  const content = manifestoContent[locale];
 
   return (
     <div className="container-custom py-12">
-      {/* Header */}
-      <div className="max-w-3xl mx-auto text-center mb-12">
+      <div className="max-w-4xl lg:max-w-5xl mx-auto text-center mb-10">
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-heading font-bold mb-4">
-          {locale === "pl" ? "Manifest" : locale === "de" ? "Manifest" : "Manifesto"}
+          {content.title}
         </h1>
       </div>
 
-      <Card className="max-w-xl mx-auto">
-        <CardContent className="py-16 text-center">
-          <Construction className="mx-auto h-16 w-16 text-primary mb-6" />
-          <h2 className="text-2xl font-heading font-semibold mb-4">
-            {locale === "pl" ? "W budowie" : locale === "de" ? "Im Aufbau" : "Under Construction"}
-          </h2>
-          <p className="text-muted-foreground text-lg">
-            {locale === "pl" 
-              ? "Nasz manifest jest obecnie przygotowywany. Wróć wkrótce." 
-              : locale === "de"
-                ? "Unser Manifest wird derzeit vorbereitet. Schauen Sie bald wieder vorbei."
-                : "Our manifesto is currently being prepared. Check back soon."}
-          </p>
+      <Card className="max-w-4xl lg:max-w-5xl mx-auto">
+        <CardContent className="py-10 md:py-12">
+          <div className="space-y-6 md:space-y-7 text-left">
+            {content.paragraphs.map((p, idx) => (
+              <p key={idx} className="text-foreground/95 text-lg md:text-xl leading-relaxed">
+                {p}
+              </p>
+            ))}
+          </div>
         </CardContent>
       </Card>
     </div>
   );
 }
+
